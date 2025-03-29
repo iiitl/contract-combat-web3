8329cd3a3d55ec1032bcacce169b77d6db85c094
datmedevil17 <143246160+datmedevil17@users.noreply.github.com>
2025-03-29 16:55:21 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,7 +5,7 @@ class MyContract:
     variable: str
 
     def __init__(self):
-        self.variable = "Ranjni beats Tanmay"
+        self.variable = "hello"
 
     @gl.public.view
     def read_method(self) -> str:
@@ -13,4 +13,4 @@ class MyContract:
 
     @gl.public.write
     def write_method(self,new_value:str) -> None:
-        self.variable = "Tanmay Died"
+        self.variable = "world"
