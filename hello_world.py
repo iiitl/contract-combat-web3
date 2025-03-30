2ab6d202f005a016e7bb18a4bec1ff7deb9cb3cd
Rohangarg <garg20005rohan@gmail.com>
2025-03-30 17:07:29 +0530

    Fixed the bug

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,17 @@
 from genlayer import *
-
+ 
 @gl.contract
 class MyContract:
-    variable: str
 
-    def __init__(self):
-        self.variable = "hello"
-
-    @gl.public.view
-    def read_method(self) -> str:
-        return self.variable
-
-    @gl.public.write
-    def write_method(self,new_value:str) -> None:
-        self.variable = "world"
+       variable: str
+ 
+       def __init__(self):
+           self.variable = "initial value"
+ 
+       @gl.public.view
+       def read_method(self) -> str:
+           return self.variable
+ 
+       @gl.public.write
+       def write_method(self, new_value: str):
+           self.variable = new_value
